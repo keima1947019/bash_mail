18952a955f1edd5aacd0c4642d4348754757b1ec
keima1947019 <cindyrotus@gmail.com>
2023-07-05 17:36:00 +0900

    20230705-173552

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bashmeil
-.
+# bash_mail
+Send individual emails to the destination email addresses listed in atesaki.csv.
--- mail.sh
+++ mail.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
-# メール送信元アドレス
-FROMADDRESS='s.john@fsolution.co.jp'
+# e-mail source address
+FROMADDRESS='master@example.com'
 
-# メール送信元の人の名前
+# personal name with e-mail source address
 FROMNAME='John Smith'
 
-# 実行前にワンクッションおく。readコマンドで$strに何か代入しているけどこのスクリプト内で$strが使われることは永遠にない。
-echo "atesaki.csvを使ってメール並列個別送信します。"
-echo "心の準備ができたらエンターキーを押して並列個別送信を実行下さい。"
+# This $str will never be used.
+echo "When you are ready, press the Enter key to execute the parallel individual transmission."
 read -p "Hit the enter key! : " str
 
-# メール件名
-MAILSUBJECT='パスワード変更通知'
+# email subject
+MAILSUBJECT='<mail subject strings>'
+
+# email authentication info
+SMTPSERVER='<smtp server FQDN>'
+SMTPPORT='<tcp port number which smtp server uses>'
+SMTPUSER='<smtp auth username>'
+SMTPPASS='<smtp auth password strings>'
 
 i=0
 while read line
 do
-
-# atesaki.csvの1行目を読み込んだ時だけスキップ
-if [ $i = 0 ] ; then
-  i=$((i += 1))
-  continue
-fi
+  # Skip only when the first line of atesaki.csv is read
+  if [ $i = 0 ] ; then
+    i=$((i += 1))
+    continue
+  fi
 
   NAME=$(awk -F',' '{print $1}' <<<${line})
   ADDRESS=$(awk -F',' '{print $2}' <<<${line})
-  PASSWORD=$(awk -F',' '{print $3}' <<<${line})
+  MSG=$(awk -F',' '{print $3}' <<<${line})
   cat <<EOF | mail -s ${MAILSUBJECT} \
-    -S smtp=smtp://mail.fsolution.co.jp:587 \
+    -S smtp=smtp://${SMTPSERVER}:${SMTPPORT} \
     -S smtp-auth=login \
-    -S smtp-auth-user=send-svinfo@fsolution.co.jp \
-    -S smtp-auth-password=fs0123456789 \
+    -S smtp-auth-user=${SMTPUSER} \
+    -S smtp-auth-password=${SMTPPASS} \
     -r $FROMADDRESS \
     $ADDRESS
 ${NAME}さん
 
-お疲れ様です。
-情報システム部 $FROMNAMEです。
+お疲れ様です。${FROMNAME}です。
 
-この度、パスワードポリシー変更に伴い、
-${NAME}さんのパスワードが変更になりましたのでお知らせします。
-新しいパスワードは「 ${PASSWORD} 」です。
-
-パスワードの取り扱いについては慎重にお願いします。
+${MSG}
 
 どうぞよろしくお願いします。
 EOF
